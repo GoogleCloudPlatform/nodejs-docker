ceb6fa06bdc0d6f587a23a4c9333e3582be943dd
Nick Kubala <nkubala@google.com>
2016-11-10 14:21:18 -0800

    fix jenkins script

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -2,16 +2,16 @@ RUNTIME_NAME="nodejs"
 
 CANDIDATE_NAME=`date +%Y-%m-%d_%H_%M`
 echo "CANDIDATE_NAME:${CANDIDATE_NAME}"
-IMAGE_NAME="${DOCKER_NAMESPACE}/${RUNTIME_NAME}:${CANDIDATE_NAME}"
+export IMAGE="${DOCKER_NAMESPACE}/${RUNTIME_NAME}:${CANDIDATE_NAME}"
 
 envsubst < base/cloudbuild.yaml.in > base/cloudbuild.yaml
 
-gcloud beta container builds submit --config base/cloudbuild.yaml .
+gcloud alpha container builds create --config base/cloudbuild.yaml .
 
 if [ "${UPLOAD_TO_STAGING}" = "true" ]; then
-  gcloud beta container images add-tag ${IMAGE_NAME} ${DOCKER_NAMESPACE}/${RUNTIME_NAME}:staging -q
+  gcloud alpha container images add-tag ${IMAGE} ${DOCKER_NAMESPACE}/${RUNTIME_NAME}:staging -q
 fi
 
 if [ ! -z "${NODE_VERSION_TAG}" ]; then
-  gcloud beta container images add-tag ${IMAGE_NAME} ${DOCKER_NAMESPACE}/${RUNTIME_NAME}:${NODE_VERSION_TAG} -q
+  gcloud alpha container images add-tag ${IMAGE} ${DOCKER_NAMESPACE}/${RUNTIME_NAME}:${NODE_VERSION_TAG} -q
 fi
