f30132d2e0dac3f6605b77fd91c12d8390ebb0d9
Kelvin Jin <kelvinjin@google.com>
2016-12-01 14:14:13 -0600

    Update jenkins_build.sh

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -9,9 +9,9 @@ envsubst < base/cloudbuild.yaml.in > base/cloudbuild.yaml
 gcloud beta container builds submit --config base/cloudbuild.yaml .
 
 if [ "${UPLOAD_TO_STAGING}" = "true" ]; then
-  gcloud beta container images add-tag ${IMAGE_NAME} ${DOCKER_NAMESPACE}/${RUNTIME_NAME}:staging -q
+  gcloud beta container images add-tag ${IMAGE} ${DOCKER_NAMESPACE}/${RUNTIME_NAME}:staging -q
 fi
 
 if [ ! -z "${NODE_VERSION_TAG}" ]; then
-  gcloud beta container images add-tag ${IMAGE_NAME} ${DOCKER_NAMESPACE}/${RUNTIME_NAME}:${NODE_VERSION_TAG} -q
+  gcloud beta container images add-tag ${IMAGE} ${DOCKER_NAMESPACE}/${RUNTIME_NAME}:${NODE_VERSION_TAG} -q
 fi
