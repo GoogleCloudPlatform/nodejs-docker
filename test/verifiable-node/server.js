f544bcad97e8d2f7557d1e476559262202651b00
Dominic Kramer <DominicDKramer@google.com>
2016-12-19 14:06:33 -0800

    Consolidated duplicate code in the unit tests

--- test/verifiable-node/server.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var http = require('http');
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end(process.version);
-}).listen(8080);
