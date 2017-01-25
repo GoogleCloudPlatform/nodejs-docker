3e77d08ec139efa3bebd319d416ffcefe67ac4d7
Dominic Kramer <DominicDKramer@google.com>
2017-01-25 00:04:13 -0800

    Files are placed in /opt/google_appengine/runtime/
    Previously, support files were placed in /opt/nodejs/.

--- test/verify-fail-on-invalid-data/run.sh
+++ test/verify-fail-on-invalid-data/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-(/opt/nodejs/verify_node /app/node-v6.9.2-linux-x64.tar.gz /app/SHASUMS256.txt.asc &> /dev/null) || echo "Correctly failed verification"
+(/opt/google_appengine/runtime/verify_node /app/node-v6.9.2-linux-x64.tar.gz /app/SHASUMS256.txt.asc &> /dev/null) || echo "Correctly failed verification"
