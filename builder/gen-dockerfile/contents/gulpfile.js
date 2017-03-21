1c8ae193120b89eb3c9293b6458ab5cdae51a608
Dominic Kramer <DominicDKramer@google.com>
2017-03-21 12:01:07 -0700

    Add copyright statements

--- builder/gen-dockerfile/contents/gulpfile.js
+++ builder/gen-dockerfile/contents/gulpfile.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var del = require('del');
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
@@ -14,9 +30,9 @@ gulp.task('copy-data', ['clean'], function() {
 });
 
 gulp.task('default', ['copy-data'], function() {
-    return tsProject.src()
-                    .pipe(sourcemaps.init())
-                    .pipe(tsProject())
-                    .pipe(sourcemaps.write())
-                    .pipe(gulp.dest('dist'));
+  return tsProject.src()
+                  .pipe(sourcemaps.init())
+                  .pipe(tsProject())
+                  .pipe(sourcemaps.write())
+                  .pipe(gulp.dest('dist'));
 });
