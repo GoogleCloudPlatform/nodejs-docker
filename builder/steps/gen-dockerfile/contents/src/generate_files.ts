40b8130c653858b24be64b80b1f51e1fe2714c35
Dominic Kramer <DominicDKramer@google.com>
2017-04-06 22:45:29 -0700

    The Dockerfile template is now in data/Dockerfile.txt

--- builder/steps/gen-dockerfile/contents/src/generate_files.ts
+++ builder/steps/gen-dockerfile/contents/src/generate_files.ts
@@ -17,78 +17,10 @@
 import * as dot from 'dot';
 dot.templateSettings.strip = false;
 
+import * as path from 'path';
+
 import {Setup} from './detect_setup';
-import {Writer} from './fsview';
-
-const DOCKERIGNORE_TEXT = `# Copyright 2015 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-node_modules
-.dockerignore
-Dockerfile
-npm-debug.log
-yarn-error.log
-.git
-.hg
-.svn
-`;
-
-const DOCKERFILE_TEMPLATE = dot.template(
-    `# Dockerfile extending the generic Node image with application files for a
-# single application.
-FROM {{= it.baseImage }}
-{{? it.config.nodeVersion }}
-# Check to see if the the version included in the base runtime satisfies
-# '{{= it.config.nodeVersion }}' and, if not, install a version of Node.js that does satisfy it.
-RUN /usr/local/bin/install_node '{{= it.config.nodeVersion }}'
-{{??}}
-{{?}}
-{{? it.config.useYarn }}
-# You have to specify "--unsafe-perm" with npm install
-# when running as root.  Failing to do this can cause
-# install to appear to succeed even if a preinstall
-# script fails, and may have other adverse consequences
-# as well.
-RUN npm install --unsafe-perm --global yarn
-{{?}}
-COPY . /app/
-{{? it.config.canInstallDeps }}
-{{? it.config.useYarn }}
-RUN yarn install --production || \\
-  ((if [ -f yarn-error.log ]; then \\
-      cat yarn-error.log; \\
-    fi) && false)
-{{??}}
-# You have to specify "--unsafe-perm" with npm install
-# when running as root.  Failing to do this can cause
-# install to appear to succeed even if a preinstall
-# script fails, and may have other adverse consequences
-# as well.
-# This command will also cat the npm-debug.log file after the
-# build, if it exists.
-RUN npm install --unsafe-perm || \\
-  ((if [ -f npm-debug.log ]; then \\
-      cat npm-debug.log; \\
-    fi) && false)
-{{?}}
-{{?}}
-{{? it.config.gotScriptsStart }}
-CMD {{= it.tool }} start
-{{??}}
-CMD node server.js
-{{?}}
-`);
+import {Writer, FsView} from './fsview';
 
 /**
  * Generates a single file and records that the file was generated as well as
@@ -131,17 +63,26 @@ export async function generateFiles(
     appDirWriter: Writer, config: Setup, baseImage: string):
     Promise<Map<string, string>> {
       const genFiles = new Map();
+      const dataDirReader = new FsView(path.join(__dirname, '../data'));
+
+      const dockerfileTemplate = await dataDirReader.read('Dockerfile.txt');
+      const generateDockerfile = dot.template(dockerfileTemplate);
+
+      // Generate the Dockerfile and remove any empty lines
+      const dockerfile = generateDockerfile({
+            baseImage: baseImage,
+            tool: config.useYarn ? 'yarn' : 'npm',
+            config: config
+          }).replace(/^\s*\n/gm, '');
+
+      const dockerignore = await dataDirReader.read('dockerignore');
 
       // Generate the Dockerfile and .dockerignore files
       await generateSingleFile(
           appDirWriter, genFiles, 'Dockerfile',
-          DOCKERFILE_TEMPLATE({
-            baseImage: baseImage,
-            tool: config.useYarn ? 'yarn' : 'npm',
-            config: config
-          }).replace(/^\s*\n/gm, ''));
+          dockerfile);
       await generateSingleFile(
-          appDirWriter, genFiles, '.dockerignore', DOCKERIGNORE_TEXT);
+          appDirWriter, genFiles, '.dockerignore', dockerignore);
 
       return genFiles;
     }
