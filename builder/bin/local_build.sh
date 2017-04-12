7ee5eb867c3f5e6684e5f67a8f840877dc5ae5ac
Dominic Kramer <DominicDKramer@google.com>
2017-04-11 21:39:42 -0700

    Change the repository of runtime builder steps (#92)
    Now runtime builder steps will reside in:
    ```
       gcr.io/gcp-runtimes/nodejs
    ```

--- builder/bin/local_build.sh
+++ builder/bin/local_build.sh
@@ -15,12 +15,12 @@ UPLOAD_TO_STAGING="false"
 ${BASE}/bin/build.sh ${BUILDER_NAMESPACE} ${BUILDER_TAG} ${UPLOAD_TO_STAGING}
 
 # Tag the built image as 'latest'
-TAG_OF_LATEST=$(gcloud beta container images list-tags ${BUILDER_NAMESPACE}/nodejs-gen-dockerfile --sort-by=~timestamp --limit=1 --format='get(tags)')
-gcloud --quiet beta container images add-tag ${BUILDER_NAMESPACE}/nodejs-gen-dockerfile:${TAG_OF_LATEST} ${BUILDER_NAMESPACE}/nodejs-gen-dockerfile:latest
+TAG_OF_LATEST=$(gcloud beta container images list-tags ${BUILDER_NAMESPACE}/nodejs/gen-dockerfile --sort-by=~timestamp --limit=1 --format='get(tags)')
+gcloud --quiet beta container images add-tag ${BUILDER_NAMESPACE}/nodejs/gen-dockerfile:${TAG_OF_LATEST} ${BUILDER_NAMESPACE}/nodejs/gen-dockerfile:latest
 
 rm -Rf ${LOCAL_BUILDS}
 mkdir ${LOCAL_BUILDS}
 
 # Store a version of nodejs.yaml that is updated to use the current gcloud's project's GCR
-sed "s|gcr.io/gcp-runtimes/nodejs-gen-dockerfile:latest|${BUILDER_NAMESPACE}/nodejs-gen-dockerfile:latest|g" ${BASE}/nodejs.yaml > ${LOCAL_BUILDS}/nodejs-${BUILDER_TAG}.yaml
+sed "s|gcr.io/gcp-runtimes/nodejs/gen-dockerfile:latest|${BUILDER_NAMESPACE}/nodejs/gen-dockerfile:latest|g" ${BASE}/nodejs.yaml > ${LOCAL_BUILDS}/nodejs-${BUILDER_TAG}.yaml
 echo ${BUILDER_TAG} > ${LOCAL_BUILDS}/nodejs.version
