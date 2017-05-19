2843ab659b48755b323ea0b4f162e66c64764425
Dominic Kramer <DominicDKramer@google.com>
2017-05-19 16:28:29 -0700

    Add a `test` task to gulp (#99)
    Now running `gulp test` will transpile the code and run all of
    the tests.
    
    PR-URL: https://github.com/GoogleCloudPlatform/nodejs-docker/pull/99

--- builder/steps/gen-dockerfile/contents/gulpfile.js
+++ builder/steps/gen-dockerfile/contents/gulpfile.js
@@ -16,22 +16,32 @@
 
 require('source-map-support').install();
 
-var del = require('del');
-var gulp = require('gulp');
+var mocha = require('gulp-mocha');
 var clangFormat = require('clang-format');
+var del = require('del');
 var format = require('gulp-clang-format');
+var gulp = require('gulp');
+var merge = require('merge2');
 var sourcemaps = require('gulp-sourcemaps');
 var ts = require('gulp-typescript');
+var tslint = require('gulp-tslint');
 
-var allFiles = ['src/**/*.ts', 'test/**/*.ts', '*.js'];
+var outDir = 'dist';
+var sources = ['src/**/*.ts'];
+var tests = ['test/**/*.ts'];
+var allFiles = ['*.js'].concat(sources, tests);
 
-gulp.task('check-format', function() {
+var exitOnError = true;
+function onError() {
+  if (exitOnError) {
+    process.exit(1);
+  }
+}
+
+gulp.task('test.check-format', function() {
   return gulp.src(allFiles)
       .pipe(format.checkFormat('file', clangFormat))
-      .on('warning', function(w) {
-        process.stderr.write(w.message);
-        process.exit(1);
-      });
+      .on('warning', onError);
 });
 
 gulp.task('format', function() {
@@ -40,34 +50,59 @@ gulp.task('format', function() {
       .pipe(gulp.dest('.'));
 });
 
+gulp.task('test.check-lint', function() {
+  return gulp.src(allFiles)
+      .pipe(tslint({formatter: 'verbose'}))
+      .pipe(tslint.report())
+      .on('warning', onError);
+});
+
 gulp.task('clean', function() {
-  return del('dist/**/*');
+  return del([outDir]);
 });
 
-gulp.task('copy-package-json', ['clean'], function() {
-  return gulp.src('package.json').pipe(gulp.dest('dist'));
+gulp.task('copy-package-json', function() {
+  return gulp.src('package.json').pipe(gulp.dest(outDir));
 });
 
-gulp.task('copy-data', ['copy-package-json'], function() {
-  return gulp.src('data/**/*').pipe(gulp.dest('dist/data'));
+gulp.task('copy-data', function() {
+  return gulp.src('data/**/*').pipe(gulp.dest(outDir + '/data'));
 });
 
-gulp.task('compile-test', ['copy-data'], function() {
-  return gulp.src(['test/**/*.ts'])
+gulp.task('compile', ['copy-data', 'copy-package-json'], function() {
+  var tsResult = gulp.src(sources)
+                     .pipe(sourcemaps.init())
+                     .pipe(ts.createProject('tsconfig.json')())
+                     .on('error', onError);
+  return merge([
+    tsResult.dts.pipe(gulp.dest(outDir + '/definitions')),
+    tsResult.js
+        .pipe(sourcemaps.write(
+            '.', {includeContent: false, sourceRoot: '../../src'}))
+        .pipe(gulp.dest(outDir + '/src')),
+    tsResult.js.pipe(gulp.dest(outDir + '/src'))
+  ]);
+});
+
+gulp.task('test.compile', ['compile'], function() {
+  return gulp.src(tests, {base: '.'})
       .pipe(sourcemaps.init())
       .pipe(ts.createProject('tsconfig.json')())
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest('dist/test'));
+      .on('error', onError)
+      .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../..'}))
+      .pipe(gulp.dest(outDir + '/'));
 });
 
-gulp.task('compile-src', ['compile-test'], function() {
-  return gulp.src(['src/**/*.ts'])
-      .pipe(sourcemaps.init())
-      .pipe(ts.createProject('tsconfig.json')())
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest('dist/src'));
+gulp.task('test.unit', ['test.compile'], function() {
+  return gulp.src([outDir + '/test/**/*.js']).pipe(mocha({reporter: 'spec'}));
 });
 
-gulp.task('compile', ['compile-src']);
+gulp.task('watch', function() {
+  exitOnError = false;
+  gulp.start(['test.compile']);
+  // TODO: also run unit tests in a non-fatal way
+  return gulp.watch(allFiles, ['test.compile']);
+});
 
+gulp.task('test', ['test.unit', 'test.check-format', 'test.check-lint']);
 gulp.task('default', ['compile']);
