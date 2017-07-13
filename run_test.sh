f39e606f857bd71342200d0c80b3076e92f1c743
Dominic Kramer <DominicDKramer@google.com>
2017-07-13 09:04:06 -0700

    Rename the `base` directory to `runtime-image` (#114)
    PR-URL: https://github.com/GoogleCloudPlatform/nodejs-docker/pull/114

--- run_test.sh
+++ run_test.sh
@@ -20,7 +20,7 @@ set -e
 # Change the current working directory to the directory containing this script
 pushd $(dirname $0) > /dev/null
 
-pushd base/bin > /dev/null
+pushd runtime-image/bin > /dev/null
 ./run_test.sh
 popd > /dev/null
 
