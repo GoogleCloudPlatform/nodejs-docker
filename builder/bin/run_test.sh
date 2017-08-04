8fc473b654dd7660c153cf98ecce833f5fd494e2
Dominic Kramer <DominicDKramer@google.com>
2017-08-04 13:51:48 -0700

    Add structure tests for the `gen-dockerfile` image (#119)

--- builder/bin/run_test.sh
+++ builder/bin/run_test.sh
@@ -17,12 +17,22 @@
 # Fail fast
 set -e
 
-# Enter the steps directory so that all paths can be relative to that directory
-pushd $(dirname $0)/../steps > /dev/null
+# Ensure we are in the builder directory
+pushd $(dirname $0)/.. > /dev/null
+  pushd bin > /dev/null
+    EXT_RUN_SH=ext_run.sh
+    curl https://raw.githubusercontent.com/GoogleCloudPlatform/runtimes-common/master/structure_tests/ext_run.sh > ${EXT_RUN_SH}
+    chmod +x ${EXT_RUN_SH}
+  popd > /dev/null
 
-pushd gen-dockerfile/contents/ > /dev/null
-yarn install
-yarn test
-popd > /dev/null
-
+  pushd steps > /dev/null
+    pushd gen-dockerfile > /dev/null
+      docker build -t test/gen-dockerfile .
+      ../../bin/${EXT_RUN_SH} -i test/gen-dockerfile -v --config test_config.yaml
+      pushd contents/ > /dev/null
+        yarn install
+        yarn test
+      popd > /dev/null
+    popd > /dev/null
+  popd > /dev/null
 popd > /dev/null
