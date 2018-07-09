77a012f1d2cb9f6c2e506dfa1e078d0e60daab8e
Dominic Kramer <DominicDKramer@google.com>
2018-07-09 11:58:14 -0700

    chore: fix the tests

--- runtime-image/test/test.ts
+++ runtime-image/test/test.ts
@@ -11,7 +11,7 @@ const BUILD_TIMEOUT_MS = 5 * 60 * 1000;
 const PORT = 8080;
 
 const host = process.env.DOCKER_HOST ?
-    process.env.DOCKER_HOST.split('//')[1].split(':')[0] :
+    process.env.DOCKER_HOST!.split('//')[1].split(':')[0] :
     'localhost';
 
 const DOCKER = new Docker(
@@ -32,10 +32,14 @@ pub   4096R/7434390BDBE9B9C5 2015-07-21 [expires: 2019-07-21]
 uid                          Colin Ihrig <cjihrig@gmail.com>
 sub   4096R/4FEC3ECC9B596CE2 2015-07-21 [expires: 2019-07-21]
 
-pub   4096R/09FE44734EB7990E 2014-04-01 [expires: 2024-03-29]
-uid                          Jeremiah Senkpiel <fishrock123@rocketmail.com>
-uid                          keybase.io/fishrock <fishrock@keybase.io>
-sub   4096R/45F5EEBD813DAE8E 2014-04-01 [expires: 2024-03-29]
+pub   4096R/B63B535A4C206CA9 2015-12-17 [expires: 2019-12-17]
+uid                          Evan Lucas <evanlucas@me.com>
+uid                          Evan Lucas <evanlucas@keybase.io>
+sub   4096R/A39CBCAE8D765781 2015-12-17 [expires: 2019-12-17]
+
+pub   4096R/B01FBB92821C587A 2016-10-07
+uid                          Gibson Fahnestock <gibfahn@gmail.com>
+sub   4096R/919AC8A92C482931 2016-10-07
 
 pub   4096R/C97EC7A07EDE3FC1 2014-11-10
 uid                          keybase.io/jasnell <jasnell@keybase.io>
@@ -44,10 +48,14 @@ uid                          James M Snell <jasnell@us.ibm.com>
 sub   2048R/7341B15C070877AC 2014-11-10 [expires: 2022-11-08]
 sub   2048R/8975BA8B6100C6B1 2014-11-10 [expires: 2022-11-08]
 
-pub   2048R/C273792F7D83545D 2013-11-18
-uid                          Rod Vagg <rod@vagg.org>
-uid                          Rod Vagg <r@va.gg>
-sub   2048R/1BDC911B8B6AED76 2013-11-18
+pub   4096R/09FE44734EB7990E 2014-04-01 [expires: 2024-03-29]
+uid                          Jeremiah Senkpiel <fishrock123@rocketmail.com>
+uid                          keybase.io/fishrock <fishrock@keybase.io>
+sub   4096R/45F5EEBD813DAE8E 2014-04-01 [expires: 2024-03-29]
+
+pub   4096R/770F7A9A5AE15600 2016-04-07
+uid                          Micha�l Zasso (Targos) <targos@protonmail.com>
+sub   4096R/4708964F8085DFE7 2016-04-07
 
 pub   4096R/E73BC641CC11F4C8 2016-01-12
 uid                          Myles Borins <myles.borins@gmail.com>
@@ -57,14 +65,10 @@ uid                          Myles Borins (Not used after January 2017) <mborins
 sub   2048R/DEA16371974031A5 2016-01-12 [expires: 2024-01-10]
 sub   2048R/933B01F40B5CA946 2016-01-12 [expires: 2024-01-10]
 
-pub   4096R/B63B535A4C206CA9 2015-12-17 [expires: 2019-12-17]
-uid                          Evan Lucas <evanlucas@me.com>
-uid                          Evan Lucas <evanlucas@keybase.io>
-sub   4096R/A39CBCAE8D765781 2015-12-17 [expires: 2019-12-17]
-
-pub   4096R/23EFEFE93C4CFFFE 2017-01-23 [expires: 2033-01-19]
-uid                          Italo A. Casas <me@italoacasas.com>
-sub   4096R/D3C55C2AAEC2131D 2017-01-23 [expires: 2033-01-19]
+pub   2048R/C273792F7D83545D 2013-11-18
+uid                          Rod Vagg <rod@vagg.org>
+uid                          Rod Vagg <r@va.gg>
+sub   2048R/1BDC911B8B6AED76 2013-11-18
 
 pub   2048R/6D5A82AC7E37093B 2015-02-03
 uid                          Christopher Dickinson <christopher.s.dickinson@gmail.com>
@@ -74,6 +78,10 @@ pub   2048R/B0A78B0A6C481CF6 2010-08-31
 uid                          isaacs (http://blog.izs.me/) <i@izs.me>
 sub   2048R/12DAF9ECEDE8123E 2010-08-31
 
+pub   4096R/23EFEFE93C4CFFFE 2017-01-23 [expires: 2033-01-19]
+uid                          Italo A. Casas <me@italoacasas.com>
+sub   4096R/D3C55C2AAEC2131D 2017-01-23 [expires: 2033-01-19]
+
 pub   4096R/50A3051F888C628D 2015-01-08 [expires: 2019-01-08]
 uid                          Julien Gilli <jgilli@fastmail.fm>
 sub   4096R/926EC77D21D4BD24 2015-01-08 [expires: 2019-01-08]
@@ -82,11 +90,7 @@ pub   1024D/7D33FF9D0246406D 2006-01-18 [expired: 2016-03-26]
 uid                          Timothy J Fontaine (Personal) <tjfontaine@gmail.com>
 uid                          Timothy J Fontaine (OFTC) <tjfontaine@oftc.net>
 uid                          Timothy J Fontaine (Work) <tj.fontaine@joyent.com>
-uid                          Timothy J Fontaine (Personal Key) <tjfontaine@atxconsulting.com>
-
-pub   4096R/B01FBB92821C587A 2016-10-07
-uid                          Gibson Fahnestock <gibfahn@gmail.com>
-sub   4096R/919AC8A92C482931 2016-10-07`;
+uid                          Timothy J Fontaine (Personal Key) <tjfontaine@atxconsulting.com>`;
 
 interface TestConfig {
   description: string;
