6a3310bf1d8169b623958df8caba97c65fd3e1ae
Don McCasland <donmccasland@google.com>
2019-03-19 15:11:23 -0700

    Adding build tools

---
+++ tools/ci-scripts/release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+export KOKORO_GITHUB_DIR=${KOKORO_ROOT}/src/github
+source ${KOKORO_GFILE_DIR}/kokoro/common.sh
+
+cd ${KOKORO_GITHUB_DIR}
+./runtime-image/bin/build.sh --docker-namespace="${DOCKER_NAMESPACE}" \
+                             --candidate-name="${TAG}" \
+                             --node-version="${NODE_VERSION_TAG}" \
+                             --upload-to-staging="${UPLOAD_TO_STAGING}" \
+                             --verbose
+METADATA=$(pwd)/METADATA
+cd ${KOKORO_GFILE_DIR}/kokoro
+python note.py nodejs -m ${METADATA} -t ${TAG}
---
+++ tools/ci-scripts/runtime-builder-steps.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export KOKORO_GITHUB_DIR=${KOKORO_ROOT}/src/github
+source ${KOKORO_GFILE_DIR}/kokoro/common.sh
+
+cd ${KOKORO_GITHUB_DIR}
+./builder/bin/build.sh ${BUILDER_NAMESPACE} ${BUILDER_TAG} ${UPLOAD_TO_STAGING}
