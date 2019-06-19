dd69e3b85f099c3616f3aaae22a8ae6453c3c8b8
Sharif Elgamal <selgamal@google.com>
2019-06-19 11:43:49 -0700

    Fix runtime builder CI script

--- tools/ci-scripts/runtime-builder-steps.sh
+++ tools/ci-scripts/runtime-builder-steps.sh
@@ -2,5 +2,5 @@
 export KOKORO_GITHUB_DIR=${KOKORO_ROOT}/src/github
 source ${KOKORO_GFILE_DIR}/kokoro/common.sh
 
-cd ${KOKORO_GITHUB_DIR}
+cd ${KOKORO_GITHUB_DIR}/nodejs-docker
 ./builder/bin/build.sh ${BUILDER_NAMESPACE} ${BUILDER_TAG} ${UPLOAD_TO_STAGING}
