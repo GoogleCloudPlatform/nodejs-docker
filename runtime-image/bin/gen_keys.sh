f0cb74b7e92714e26c8a2e885e3c6636df34ce7b
Paul Feng <35683264+paul-feng-github@users.noreply.github.com>
2022-10-18 14:12:49 -0700

    Update gen_keys.sh
    Add the missing release key from Bryan English to gen_keys.sh. This should unblock the build against the latest nodejs version.
    
    This is to address the issue reported at https://buganizer.corp.google.com/issues/254111235

--- runtime-image/bin/gen_keys.sh
+++ runtime-image/bin/gen_keys.sh
@@ -24,15 +24,17 @@ BryanEnglish=141F07595B7B3FFE74309A937405533BE57C7D57
 ColinIhrig=94AE36675C464D64BAFA68DD7434390BDBE9B9C5
 DanielleAdams=74F12602B6F1C4E913FAA37AD3A89613643B6201
 JamesMSnell=71DCFD284A79C3B38668286BC97EC7A07EDE3FC1
+JuanJoseArboleda=61FC681DFB92A079F1685E77973F295594EC4689
 MichaelZasso=8FCCA13FEF1D0C2E91008E09770F7A9A5AE15600
 MylesBorins=C4F0DFFF4E8C1A8236409D08E73BC641CC11F4C8
+RafaelGSS=890C08DB8579162FEE0DF9DB8BEAB4DFCF555EF4
 RichardLau=C82FA3AE1CBEDC6BE46B9360C43CEC45C17AB93C
 RodVagg=DD8F2338BAE7501E3DD5AC78C273792F7D83545D
 RubenBridgewater=A48C2BEE680E841632CD4E44F07496B3EB3C1762
 RuyAdorno=108F52B48DB57BB0CC439B2997B01419BD92F80A
 ShelleyVohr=B9E2F5981AA6E0CD28160D9FF13993A75599653C
 
-keylist="${EvanLucas} ${GibsonFahnestock} ${JeremiahSenkpiel} ${ChrisDickinson} ${IsaacZSchlueter} ${ItaloACasas} ${JulienGilli} ${TimothyJFontaine} ${DanielleAdamsOld} ${BethGriggs} ${BryanEnglish} ${ColinIhrig} ${DanielleAdams} ${JamesMSnell} ${MichaelZasso} ${MylesBorins} ${RichardLau} ${RodVagg} ${RubenBridgewater} ${RuyAdorno} ${ShelleyVohr}"
+keylist="${EvanLucas} ${GibsonFahnestock} ${JeremiahSenkpiel} ${ChrisDickinson} ${IsaacZSchlueter} ${ItaloACasas} ${JulienGilli} ${TimothyJFontaine} ${DanielleAdamsOld} ${BethGriggs} ${BryanEnglish} ${ColinIhrig} ${DanielleAdams} ${JamesMSnell} ${JuanJoseArboleda} ${MichaelZasso} ${MylesBorins} ${RafaelGSS} ${RichardLau} ${RodVagg} ${RubenBridgewater} ${RuyAdorno} ${ShelleyVohr}"
 
 for key in ${keylist} ; do
   gpg2 --keyserver hkp://pgp.mit.edu:11371 --recv-keys ${key}
